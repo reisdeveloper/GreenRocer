862c8bc7ea949c1ef38c9337f1a1a12958208ee9
devreis <devjhonatanreis@gmail.com>
2024-02-09 23:45:25 -0300

    addproductscreen

--- lib/src/screens/product/product_screen.dart
+++ lib/src/screens/product/product_screen.dart
@@ -1,19 +1,115 @@
 import 'package:flutter/material.dart';
 import 'package:greengrocer/src/config/custom_colors.dart';
 import 'package:greengrocer/src/models/item_model.dart';
+import 'package:greengrocer/src/services/utils_services.dart';
 
 class ProductScreen extends StatelessWidget {
-  const ProductScreen({Key? key, required this.item}) : super(key: key);
+  ProductScreen({Key? key, required this.item}) : super(key: key);
 
   final ItemModel item;
+  final UtilsServices utilsServices = UtilsServices();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: CustomColors.white.withAlpha(230),
-      body: Column(
+      body: Stack(
         children: [
-          Expanded(
-            child: Image.asset(item.imgUrl),
+          Positioned(
+            child: SafeArea(
+                child: IconButton(
+              onPressed: () {
+                Navigator.of(context).pop('');
+              },
+              icon: const Icon(Icons.arrow_back),
+            )),
+          ),
+          Column(
+            children: [
+              Expanded(
+                child: Image.asset(item.imgUrl),
+              ),
+              Expanded(
+                child: Container(
+                  padding: const EdgeInsets.all(32),
+                  decoration: BoxDecoration(
+                    color: CustomColors.white,
+                    borderRadius: const BorderRadius.vertical(
+                      top: Radius.circular(50),
+                    ),
+                    boxShadow: [
+                      BoxShadow(
+                        color: CustomColors.grey.shade600,
+                        offset: const Offset(0, 2),
+                      ),
+                    ],
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            item.itemName,
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                            style: const TextStyle(
+                              fontSize: 27,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Container(
+                            height: 30,
+                            width: 70,
+                            color: Colors.red,
+                          )
+                        ],
+                      ),
+                      Text(
+                        utilsServices.priceToCurrency(item.price),
+                        style: TextStyle(
+                          fontSize: 23,
+                          fontWeight: FontWeight.bold,
+                          color: CustomColors.customSwatchColor,
+                        ),
+                      ),
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 10),
+                          child: SingleChildScrollView(
+                            child: Text(
+                              item.description,
+                              style: const TextStyle(height: 1.5),
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 55,
+                        child: ElevatedButton.icon(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.green,
+                            foregroundColor: CustomColors.white,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15),
+                            ),
+                          ),
+                          label: const Text(
+                            'Adicionar ao carinho',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          icon: const Icon(Icons.shopping_cart_outlined),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
         ],
       ),
